0fbec8c70dc7999194e8e8df57105385d385f2f9
ignardt <i.andres.2019@alumnos.urjc.es>
2024-06-25 20:39:23 +0200

    mejora

--- P4/main.js
+++ P4/main.js
@@ -1,59 +1,65 @@
 const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('path');
-const { networkInterfaces } = require('os');
+const os = require('os');
 
 function getLocalIP() {
-  const nets = networkInterfaces();
-  let results = {};
-  for (const name of Object.keys(nets)) {
-    for (const net of nets[name]) {
-      if (net.family === 'IPv4' && !net.internal) {
-        if (!results[name]) {
-          results[name] = [];
+    const nets = os.networkInterfaces();
+    let results = {};
+    for (const name of Object.keys(nets)) {
+        for (const net of nets[name]) {
+            if (net.family === 'IPv4' && !net.internal) {
+                if (!results[name]) {
+                    results[name] = [];
+                }
+                results[name].push(net.address);
+            }
         }
-        results[name].push(net.address);
-      }
     }
-  }
-  return results[Object.keys(results)[0]][0];
+    return results[Object.keys(results)[0]][0];
 }
 
 function createWindow() {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
-      contextIsolation: true,
-      enableRemoteModule: false,
-    }
-  });
-
-  win.loadFile('info.html');
-  win.webContents.on('did-finish-load', () => {
-    win.webContents.send('app-info', {
-      nodeVersion: process.versions.node,
-      chromeVersion: process.versions.chrome,
-      electronVersion: process.versions.electron,
-      ipAddress: getLocalIP()
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+            contextIsolation: true,
+            enableRemoteModule: false,
+        }
     });
-  });
 
-  ipcMain.on('open-chat', () => {
-    win.loadFile('index.html');
-  });
+    win.loadFile('info.html');
+    win.webContents.on('did-finish-load', () => {
+        win.webContents.send('app-info', {
+            nodeVersion: process.versions.node,
+            chromeVersion: process.versions.chrome,
+            electronVersion: process.versions.electron,
+            ipAddress: getLocalIP(),
+            platform: os.platform(),
+            architecture: os.arch(),
+            hostname: os.hostname(),
+            homeDir: os.homedir(),
+            tempDir: os.tmpdir(),
+            uptime: os.uptime(),
+        });
+    });
+
+    ipcMain.on('open-chat', () => {
+        win.loadFile('index.html');
+    });
 }
 
 app.whenReady().then(createWindow);
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
 });
 
 app.on('activate', () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
 });
--- P4/preload.js
+++ P4/preload.js
@@ -1,6 +1,6 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electron', {
-  onAppInfo: (callback) => ipcRenderer.on('app-info', (event, info) => callback(info)),
-  openChat: () => ipcRenderer.send('open-chat'),
+    onAppInfo: (callback) => ipcRenderer.on('app-info', (event, info) => callback(info)),
+    openChat: () => ipcRenderer.send('open-chat'),
 });
